c7342952865f5e84ee87093d21dffb01ba8e8708
Alex <alexf81xxx@gmail.com>
2021-01-14 12:46:33 +0300

    Json properties

--- src/main/java/com/example/currency/models/CurrencyDeserialization.java
+++ src/main/java/com/example/currency/models/CurrencyDeserialization.java
@@ -1,5 +1,7 @@
 package com.example.currency.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,17 +10,29 @@ import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+
+@JsonIgnoreProperties (ignoreUnknown = true)
 
 public class CurrencyDeserialization {
 
+    @JsonProperty ("1. From_Currency Code")
     private String fromCurrencyCode;
+    @JsonProperty ("2. From_Currency Name")
     private String fromCurrencyName;
+    @JsonIgnoreProperties("3. To_Currency Code")
     private String toCurrencyCode;
+    @JsonIgnoreProperties("4. To_Currency Name")
     private double toCurrencyName;
+    @JsonIgnoreProperties("5. Exchange Rate")
     private double exchangeRate;
+    @JsonIgnoreProperties("6. Last Refreshed")
     private LocalDateTime lastRefreshed;
-    private double timeZone;
+    @JsonProperty("7. Time Zone")
+    private String timeZone;
+    @JsonIgnoreProperties("8. Bid Price")
     private double bidPrice;
+    @JsonIgnoreProperties("9. Ask Price")
     private double askPrice;
 
 }
