6ade02dcec1d5bfa71d3e3a7d55aeab74cc1d366
Alex <alexf81xxx@gmail.com>
2021-01-14 18:36:51 +0300

    Worked without date

--- src/main/java/com/example/currency/models/CurrencyDeserialization.java
+++ src/main/java/com/example/currency/models/CurrencyDeserialization.java
@@ -16,23 +16,42 @@ import java.time.LocalDateTime;
 
 public class CurrencyDeserialization {
 
-    @JsonProperty ("1. From_Currency Code")
+   @JsonProperty ("1. From_Currency Code")
     private String fromCurrencyCode;
     @JsonProperty ("2. From_Currency Name")
     private String fromCurrencyName;
-    @JsonIgnoreProperties("3. To_Currency Code")
+    @JsonProperty("3. To_Currency Code")
     private String toCurrencyCode;
-    @JsonIgnoreProperties("4. To_Currency Name")
-    private double toCurrencyName;
-    @JsonIgnoreProperties("5. Exchange Rate")
+    @JsonProperty("4. To_Currency Name")
+    private String toCurrencyName;
+    @JsonProperty("5. Exchange Rate")
     private double exchangeRate;
-    @JsonIgnoreProperties("6. Last Refreshed")
-    private LocalDateTime lastRefreshed;
+    /*@JsonProperty("6. Last Refreshed")
+    private LocalDateTime lastRefreshed;*/
     @JsonProperty("7. Time Zone")
     private String timeZone;
-    @JsonIgnoreProperties("8. Bid Price")
+    @JsonProperty("8. Bid Price")
     private double bidPrice;
-    @JsonIgnoreProperties("9. Ask Price")
+    @JsonProperty("9. Ask Price")
     private double askPrice;
+
+    /*@JsonProperty("1. From_Currency Code")
+    private String fromCurrencyCode;
+    @JsonProperty("2. From_Currency Name")
+    private String fromCurrencyName;
+    @JsonProperty("3. To_Currency Code")
+    private String toCurrencyCode;
+    @JsonProperty("4. To_Currency Name")
+    private String toCurrencyName;
+    @JsonProperty("5. Exchange Rate")
+    private double exchangeRate;
+    //    @JsonProperty("6. Last Refreshed")
+//    private LocalDateTime lastRefreshed;
+    @JsonProperty("7. Time Zone")
+    private String timeZone;
+    @JsonProperty("8. Bid Price")
+    private double bidPrice;
+    @JsonProperty("9. Ask Price")
+    private double askPrice;*/
 
 }
